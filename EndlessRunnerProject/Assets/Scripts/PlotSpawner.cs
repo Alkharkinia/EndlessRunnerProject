07683aaa74fa874dccb45c732b0df4f0f004718a
Alkharkinia <zabrandonasi@gmail.com>
2024-11-24 18:41:58 +0300

    Lesson3

---
+++ EndlessRunnerProject/Assets/Scripts/PlotSpawner.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlotSpawner : MonoBehaviour
+{
+    private int initAmount = 5;
+    private float plotSize = 40f;
+    private float xPosLeft = -31.95f;
+    private float xPosRight = 31.95f;
+    private float lastZPos = 10f;
+
+    public List<GameObject> plots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < initAmount; i++)
+        {
+            SpawnPlot();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnPlot()
+    {
+        int rand = Random.Range(0, plots.Count);
+        GameObject plotLeft = plots[rand];
+        GameObject plotRight = plots[rand];
+
+        float zPos = lastZPos + plotSize;
+
+        Instantiate(plotLeft, new Vector3(xPosLeft, -3f, zPos), plotLeft.transform.rotation);
+        Instantiate(plotRight, new Vector3(xPosRight, -3f, zPos),  Quaternion.Euler(0, 90, 0));
+
+        lastZPos += plotSize;
+    }
+}
