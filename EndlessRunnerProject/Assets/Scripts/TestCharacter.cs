1fa3063987cfa161943424af889880f1257abd3b
Alkharkinia <zabrandonasi@gmail.com>
2024-11-24 15:04:39 +0300

    Video1

---
+++ EndlessRunnerProject/Assets/Scripts/TestCharacter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCharacter : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Optionally, initialize anything here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Use Time.deltaTime to make movement frame-rate independent
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float vMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        transform.Translate(new Vector3(hMovement, 0, vMovement));
+    }
+}
