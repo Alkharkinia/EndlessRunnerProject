60f84264ee25edc48f7e9b5dacc31aef17f04afa
Alkharkinia <zabrandonasi@gmail.com>
2024-12-15 03:31:53 +0300

    PlayerController

---
+++ EndlessRunnerProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody rb;
+
+    // Movement speeds
+    public float forwardSpeed = 5f;
+    public float strafeSpeed = 3f;
+    public float jumpForce = 7f;
+
+    // Ground check
+    private bool isGrounded = true;
+    private bool isJumping = false;
+
+    public SpawnManager spawnManager;
+
+    void Start()
+    {
+        // Get Animator and Rigidbody components
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        // Constant forward movement
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, forwardSpeed);
+
+        // Reset strafe states
+        bool strafingRight = false;
+        bool strafingLeft = false;
+
+        // Check for player input
+        if (Input.GetKey(KeyCode.A)) // Strafe Left
+        {
+            rb.velocity = new Vector3(-strafeSpeed, rb.velocity.y, forwardSpeed);
+            strafingLeft = true;
+        }
+        else if (Input.GetKey(KeyCode.D)) // Strafe Right
+        {
+            rb.velocity = new Vector3(strafeSpeed, rb.velocity.y, forwardSpeed);
+            strafingRight = true;
+        }
+        else
+        {
+            // Maintain forward speed when not strafing
+            rb.velocity = new Vector3(0, rb.velocity.y, forwardSpeed);
+        }
+
+        // Update Animator parameters
+        animator.SetBool("StrafeRight", strafingRight);
+        animator.SetBool("StrafeLeft", strafingLeft);
+
+        // Update RunForward animation
+        animator.SetBool("RunForward", !strafingRight && !strafingLeft && !isJumping);
+    }
+
+    void HandleJump()
+    {
+        // Check for jump input and ground status
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            isJumping = true;
+
+            // Update Animator parameters
+            animator.SetBool("IsJumping", true);
+        }
+
+        // Reset jump state when grounded
+        if (isGrounded && isJumping)
+        {
+            isJumping = false;
+            animator.SetBool("IsJumping", false);
+        }
+    }
+
+    // Detect ground collisions
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
--- EndlessRunnerProject/Assets/Scripts/TestCharacter.cs
+++ EndlessRunnerProject/Assets/Scripts/TestCharacter.cs
@@ -4,27 +4,104 @@ using UnityEngine;
 
 public class TestCharacter : MonoBehaviour
 {
-    public float movementSpeed = 10f;
-    public SpawnManager spawnManager;
+    public float movementSpeed = 10f;   // Speed at which the character moves
+    public float jumpForce = 5f;        // Jump force magnitude
+    public float forwardSpeed = 5f;     // Constant forward speed (vertical movement)
+    public SpawnManager spawnManager;   // Reference to the SpawnManager
+
+    private Rigidbody rb;               // Reference to the Rigidbody component
+    private Animator animator;          // Reference to the Animator component
+    private bool isGrounded = true;
+
+    // Animator Parameters
+    private bool isStrafingRight = false;
+    private bool isStrafingLeft = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Optionally, initialize anything here
+        rb = GetComponent<Rigidbody>();      // Get the Rigidbody component
+        animator = GetComponent<Animator>(); // Get the Animator component
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Use Time.deltaTime to make movement frame-rate independent
-        float hMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        float vMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+        HandleMovement();
+        HandleJump();
+        HandleStrafing();
+    }
 
+    void HandleMovement()
+    {
+        // Handle horizontal movement (left/right) with player input (A/D or Left/Right Arrow)
+        float hMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+
+        // Apply constant forward movement along the z-axis (vertical movement in Unity's world space)
+        float vMovement = forwardSpeed * Time.deltaTime; // Constant speed on the z-axis
+
+        // Move the character based on input and constant forward speed
         transform.Translate(new Vector3(hMovement, 0, vMovement));
+    }
+
+    void HandleJump()
+    {
+        // Check for jump input and if the player is grounded
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            // Apply upward force for jumping
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            // Set the Animator trigger for the jump animation
+            animator.SetTrigger("Jump");
+
+            isGrounded = false;
+
+        }
+
+        if (transform.position.y < -2.998f) { 
+
+            isGrounded = true;
+    }
+
+    }
+
+    void HandleStrafing()
+    {
+        // Right Strafe
+        if (Input.GetKeyDown(KeyCode.D)) // Replace 'D' with your desired key
+        {
+            isStrafingRight = true;
+            isStrafingLeft = false;
+
+            animator.SetBool("StrafeRight", true);
+            animator.SetBool("StrafeLeft", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            isStrafingRight = false;
+            animator.SetBool("StrafeRight", false);
+        }
+
+        // Left Strafe
+        if (Input.GetKeyDown(KeyCode.A)) // Replace 'A' with your desired key
+        {
+            isStrafingLeft = true;
+            isStrafingRight = false;
+
+            animator.SetBool("StrafeLeft", true);
+            animator.SetBool("StrafeRight", false);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            isStrafingLeft = false;
+            animator.SetBool("StrafeLeft", false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-       spawnManager.SpawnTriggerEntered();
+        // Notify the spawn manager when a trigger is entered
+        spawnManager.SpawnTriggerEntered();
     }
 }
